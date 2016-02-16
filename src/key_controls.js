24e2f80b586af7c24d9a20f2b94d59142bc39870
Jason <jason@youtorque.com>
2016-02-16 16:11:30 -0600

    Refactored to use client "class" structure. I have update and render broken out but I don't think they are decoupled yet. If I'm even going to decouple. I would like to get a single delta time variable going as well. Right now I'm calculating delta time in multiple locations.

---
+++ src/key_controls.js
@@ -0,0 +1,47 @@
+//keydown event
+document.addEventListener('keydown', function(event){
+    switch (event.keyCode){
+        case 38: // up
+        case 87: // w
+                client.key_up = true;
+                break;
+        case 37: // left
+        case 65: // a
+                client.key_left = true; 
+                break;
+        case 40: // down
+        case 83: // s
+                client.key_down = true;
+                break;
+        case 39: // right
+        case 68: // d
+                client.key_right = true;
+                break;
+        case 32: // space
+                if ( client.jump === true ) velocity.y += 350;
+                client.jump = false;
+                break;                                    
+        }
+}, false);
+
+//keyup event
+document.addEventListener('keyup', function(event){
+    switch(event.keyCode) {
+        case 38: // up
+        case 87: // w
+                client.key_up = false;
+                break;
+        case 37: // left
+        case 65: // a
+                client.key_left = false;
+                break;
+        case 40: // down
+        case 83: // s
+                client.key_down = false;
+                break;
+        case 39: // right
+        case 68: // d
+                client.key_right = false;
+                break;
+        }
+}, false);
